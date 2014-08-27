5f3d739be4460ca25579fef45779d91883b60ae5
Chai Feng <chaifeng@chaifeng.com>
2014-08-28 07:54:34 +0800

    让step的描述更灵活(不建议, 仅供演示)

--- step_definitions/login_steps.rb
+++ step_definitions/login_steps.rb
@@ -7,23 +7,24 @@ After('@logout') do
   @login_page = LoginPage.new
 end
 
-假如(/^我没有登录$/) do
+假如(/^(?:我)?没有登录$/) do
   @login_page = LoginPage.new
 end
 
-假如(/^我使用有效(?:的)?账号登录$/) do
+假如(/^(?:我)?使用有效(?:的)?账号登录$/) do
   @login_page ||= LoginPage.new
   @login_page.login User.get_default_account
 end
 
-那么(/^我可以登录成功$/) do
+那么(/^(?:我)?(?:可以|能够)(?:登录成功|成功登录)$/) do
   @login_page.assert_is_login
 end
 
-假如(/^我使用无效的账号登录$/) do
+假如(/^(?:我)?使用无效(?:的)?账号登录$/) do
+  @login_page ||= LoginPage.new
   @login_page.login User.get("无效账号")
 end
 
-那么(/^我(?:的登录会失败|会看到登录失败的消息)$/) do
+那么(/^(?:我)?(?:的登录会失败|登录会失败|会看到登录失败的消息)$/) do
   @login_page.assert_invalid_password
 end
